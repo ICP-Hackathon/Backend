1a35c294e8c7cda866a36c2e514b4e66e8878019
poqopo <block.poqopo@gmail.com>
2024-09-03 15:43:44 +0900

    Now it works!

--- AI/main.py
+++ AI/main.py
@@ -2,5 +2,5 @@ from db import faiss
 from crud import add_text
 
 
-add_text(['When you fall in love at first sight and decide to go on a date with her, don’t rush. Haste can ruin everything. It’s more important to gradually get to know her and understand her mind. Be patient.'], ['dating_adivce_ai'], ['dating_advice_ai_0'])
+# add_text(['When you fall in love at first sight and decide to go on a date with her, don’t rush. Haste can ruin everything. It’s more important to gradually get to know her and understand her mind. Be patient.'], [{"source": "dating_adivce_ai"}], ['dating_advice_ai_0'])
 print(faiss.db.index_to_docstore_id)
