ca3409a19016203e6125c33e1bd99efd8be39e4b
poqopo <block.poqopo@gmail.com>
2024-09-03 15:47:20 +0900

    API call doesn work at libray com

--- AI/main.py
+++ AI/main.py
@@ -2,5 +2,5 @@ from db import faiss
 from crud import add_text
 
 
-# add_text(['When you fall in love at first sight and decide to go on a date with her, don’t rush. Haste can ruin everything. It’s more important to gradually get to know her and understand her mind. Be patient.'], [{"source": "dating_adivce_ai"}], ['dating_advice_ai_0'])
+add_text(['In relationships between people, the most important thing is empathy. Through empathy, we can grow closer to one another. We naturally feel more drawn to those who show us genuine understanding.'], [{"source": "dating_adivce_ai"}], ['dating_advice_ai_1'])
 print(faiss.db.index_to_docstore_id)
