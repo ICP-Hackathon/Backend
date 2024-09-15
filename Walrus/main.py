90eab5587ab291b8ac2deb0f269c3bd198b5eea4
guboc11 <guboc11@gmail.com>
2024-09-15 16:45:12 +0900

    Add send_data(), read_blob()

--- Walrus/main.py
+++ Walrus/main.py
@@ -1,8 +1,19 @@
+import requests
+
+def send_data(data: str):
+  publisher = "https://publisher-devnet.walrus.space"
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
+  url = '{0}/v1/store'.format(publisher)
+  response = requests.put(url, headers=headers, data=data)
+  print(response.json())
+  return response.json()
 
 
-def send_file(file_name: str):
-  print("send ",file_name, "to walrus")
+def read_blob(blob_id: str):
+  aggregator = "https://aggregator-devnet.walrus.space"
+  response = requests.get('{0}/v1/{1}'.format(aggregator, blob_id))
+  print(response.text)
+  return response.text
 
-
-def get_blob(blob_id: str):
-  print("blob_id :", blob_id)
